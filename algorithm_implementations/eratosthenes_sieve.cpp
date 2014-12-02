16132c930a854f5ccdc918c4d5af871fdf81e953
vignesh-m <vicky.mano@gmail.com>
2014-12-02 12:12:59 +0530

    org change

--- algorithm_implementations/eratosthenes_sieve.cpp
+++ algorithm_implementations/eratosthenes_sieve.cpp
@@ -3,7 +3,6 @@
 //  algorithm_implementations
 //
 //  Created by Vignesh M on 30/11/14.
-//  Copyright (c) 2014 e-maxx. All rights reserved.
 //
 
 #include "eratosthenes_sieve.h"
--- algorithm_implementations/euclid_gcd.cpp
+++ algorithm_implementations/euclid_gcd.cpp
@@ -3,7 +3,6 @@
 //  algorithm_implementations
 //
 //  Created by Vignesh M on 30/11/14.
-//  Copyright (c) 2014 e-maxx. All rights reserved.
 //
 
 #include "euclid_gcd.h"
